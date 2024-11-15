cb9aa030e2bdcecde9df1ad65be7663b1d665745
zied23e <156387590+zied23e@users.noreply.github.com>
2024-11-15 16:44:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,24 @@
-import requests
+print("\n1. Trending PC Games")
+print("2. Top PC Titles")
+print("3. Top xbox Titles")
+print("4. Top PS Titles")
+print("5. New Releases\n")
+def func():
+    inp=input("> ")
 
-url = 'https://www.metacritic.com/game/'
-r = requests.get(url)
+    if inp == '1':
+        print("Trending")
+
+    elif inp == '2':
+        print("Trending games")
+
+    elif inp == '3':
+        print("pc")
+
+    elif inp == '4':
+        print("ps")
+
+
+    elif inp == '5':
+        print("release")
+func()
